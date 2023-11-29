07089f6f76d326ffb4214c701266dea6b525f548
Davidbassemm <davidbassem694@gmail.com>
2023-11-29 15:58:44 -0500

    a

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for digit1 in range(10):
     for digit2 in range(digit1, 10):
-        print(f"{digit1}{digit2}", end=", ")
+        if digit1 < digit2:
+            print("{:d}{:d}".format(i, j),
+                  end="\n" if digit1 == 8 and digit2 == 9 else ", ")
