a644a0bed2a9b4e82a93d69271c2fb95cfe9d0d6
Davidbassemm <davidbassem694@gmail.com>
2024-03-31 10:33:41 -0400

    a

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
+""" Finds the peak in a list of unsorted integers
+"""
 
 
 def find_peak(list_of_integers):
-
-    if list_of_integers is None or len(list_of_integers) == 0:
+    """
+    Args:
+        list_of_integers(int): list of integers to find peak of
+    Returns: peak of list_of_integers or None
+    """
+    if not list_of_integers:
         return None
 
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
+    lower, higher = 0, len(list_of_integers) - 1
 
-    mid_idx = int(len(list_of_integers) / 2)
+    while lower < higher:
+        middle = (lower + higher) // 2
 
-    if mid_idx != len(list_of_integers) - 1:
-        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and\
-           list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]:
-            return list_of_integers[mid_idx]
-    else:
-        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
-            return list_of_integers[mid_idx]
+        if list_of_integers[middle] > list_of_integers[middle + 1]:
+            higher = middle
         else:
-            return list_of_integers[mid_idx - 1]
+            lower = middle + 1
 
-    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
-        a_list = list_of_integers[0:mid_idx]
-    else:
-        a_list = list_of_integers[mid_idx + 1:]
-
-    return find_peak(a_list)
+    return list_of_integers[lower]
